4b404315f2d6aa1278922d3605911002fc07524d
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-25 12:12:54 +0530

    wadsad

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,36 +1,36 @@
-import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+// import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 
-import * as THREE from 'three'
-import Lights from './Lights';
-import { Suspense } from "react";
-import IPhone from "./IPhone";
+// import * as THREE from 'three'
+// import Lights from './Lights';
+// import { Suspense } from "react";
+// import IPhone from "./IPhone";
 
-const ModelView = ({
-  index,
-  groupRef,
-  gsapType,
-  controlRef,
-  setRotationState,
-  item,
-  size,
-}) => {
-  return (
-    <View
-      index={index}
-      id={gsapType}
-      className={`border-2 border-red-500 w-full h-full absolute ${
-        index === 2 ? "right-[-100%]" : ""
-      }`}
-    >
-      <ambientLight intensity={0.3} />
-      <PerspectiveCamera makeDefault position={[0, 0]} />
-      <Lights/>
+// const ModelView = ({
+//   index,
+//   groupRef,
+//   gsapType,
+//   controlRef,
+//   setRotationState,
+//   item,
+//   size,
+// }) => {
+//   return (
+//     <View
+//       index={index}
+//       id={gsapType}
+//       className={`border-2 border-red-500 w-full h-full absolute ${
+//         index === 2 ? "right-[-100%]" : ""
+//       }`}
+//     >
+//       <ambientLight intensity={0.3} />
+//       <PerspectiveCamera makeDefault position={[0, 0]} />
+//       <Lights/>
 
-      <Suspense fallback={<div>Loading</div>}>
-       <IPhone/>
-      </Suspense>
-    </View>
-  );
-};
+//       <Suspense fallback={<div>Loading</div>}>
+//        <IPhone/>
+//       </Suspense>
+//     </View>
+//   );
+// };
 
-export default ModelView;
+// export default ModelView;
